4ba40294b84bdf7d627b3f14fa2306e5958a68a9
AlexAegis <alexaegis@gmail.com>
2018-04-18 23:44:14 +0200

    format

--- task3/submission/abaps.cc
+++ task3/submission/abaps.cc
@@ -7,10 +7,9 @@
 
 using namespace std;
 
-void input_handler( ifstream& in, int argc, char* argv[] );
+void input_handler(ifstream& in, int argc, char* argv[]);
 
-int main( int argc, char* argv[] )
-{
+int main(int argc, char* argv[]) {
     ifstream in;
     input_handler( in, argc, argv );
     Parser prs(in);
@@ -18,16 +17,13 @@ int main( int argc, char* argv[] )
     return 0;
 }
 
-void input_handler( ifstream& in, int argc, char* argv[] )
-{
-    if( argc < 2 )
-    {
+void input_handler(ifstream& in, int argc, char* argv[]) {
+    if(argc < 2) {
         cerr << "A forditando fajl nevet parancssori parameterben kell megadni." << endl;
         exit(1);
     }
-    in.open( argv[1] );
-    if( !in )
-    {
+    in.open(argv[1]);
+    if(!in) {
         cerr << "A " << argv[1] << "fajlt nem sikerult megnyitni." << endl;
         exit(1);
     }
